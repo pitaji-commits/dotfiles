6e1dc948b829965bc1cb2c70f116c67318b4ef18
pitaji-commits <shivamjuyal2023@gmail.com>
2025-12-03 15:44:58 +0530

    initialize package system in init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,3 +3,14 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq ring-bell-function 'ignore)
+
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("gnu"   . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
