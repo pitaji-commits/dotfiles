c4e884cebbb5edf6672e127d392b25a277e71ac9
pitaji-commits <shivamjuyal2023@gmail.com>
2025-12-03 14:26:24 +0530

    bootstrap.sh added link_dir function

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,18 @@
 set -e
 
 
+link_dir() {
+	local src="$1"
+	local dest="$2"
+
+	echo "Linking $src -> $dest"
+
+	mkdir -p "$(dirname "$dest")"
+	rm -rf "$dest"
+	ln -s "$src" "$dest"
+}
+
+
 ### bashrc
 # dotfiles/bash/bashrc -> ~/.bashrc
 if [ -f "$PWD/bash/bashrc" ]; then
