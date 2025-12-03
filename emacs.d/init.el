e696b1608b958e2779276b60e3a23f85cea309a5
pitaji-commits <shivamjuyal2023@gmail.com>
2025-12-03 16:26:48 +0530

    little bit of formatting of init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,6 @@
-(setq inhibit-startup-message t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(setq ring-bell-function 'ignore)
-
+;; ────────────────────────────────────────────────────────────────────────────────
+;; Package Initialization
+;; ────────────────────────────────────────────────────────────────────────────────
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -15,5 +12,24 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+
+;; ────────────────────────────────────────────────────────────────────────────────
+;; Installed Packages
+;; ────────────────────────────────────────────────────────────────────────────────
+
 (use-package doom-themes)
 (load-theme 'doom-material-dark t)
+
+
+;; ────────────────────────────────────────────────────────────────────────────────
+;; Core Settings
+;; ────────────────────────────────────────────────────────────────────────────────
+
+(setq inhibit-startup-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq ring-bell-function 'ignore)
+
+
+
