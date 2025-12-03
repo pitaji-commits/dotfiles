261475dea62fb429a3c779e6afe190019a4e2906
pitaji-commits <shivamjuyal2023@gmail.com>
2025-12-03 17:31:02 +0530

    formatter init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,42 +44,42 @@
 ;; Core Settings
 ;; ────────────────────────────────────────────────────────────────────────────────
 
-;; UI Specific Settings
+;; --- UI Specific Settings ---
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq ring-bell-function 'ignore)
 
-;; Relative Line Numbers
+;; --- Relative Line Numbers ---
 (global-display-line-numbers-mode t)
 (setq display-line-numbers-type 'relative)
 
-;; electric pair
+;; --- electric pair ---
 (setq electric-pair-pairs '((?\( . ?\))   ; parentheses
                             (?\[ . ?\])   ; square brackets
                             (?\{ . ?\})   ; curly braces
                             (?\" . ?\")   ; double quotes
                             (?\' . ?\'))) ; single quotes
 
-;; Only pair () and "" in lisp buffer
+;; --- Only pair () and "" in lisp buffer ---
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
 	    (setq-local electric-pair-pairs '((?\( . ?\))
 					      (?\" . ?\")))))
 (electric-pair-mode 1)
 
-;; Never let point get closer than 10 lines to top/bottom of window
+;; --- Never let point get closer than 10 lines to top/bottom of window ---
 (setq scroll-margin 10)
 (setq scroll-conservatively 101)
 (setq scroll-step 1)
 
-;; Handle temporary files
+;; --- Handle temporary files ---
 (setq auto-save-default nil)  ;; Disable auto-saving
 (setq make-backup-files nil)  ;; Disable backup~ files
 (setq create-lockfiles  nil)  ;; Disable .#lock files
 
-;; Some Performance settings via ChatGPT
+;; --- Some Performance settings via ChatGPT ---
 (setq gc-cons-threshold (* 50 1000 1000))
 (add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 2 1000 1000))))
 (custom-set-variables
